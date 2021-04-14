86268149653c6ffcbc4fe08389cef5514c80db22
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-14 19:18:52 +0530

    Minor change

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
-import { useEffect, useState } from 'react';
+
 import './App.css';
-import Movie from "./components/Movie";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Home from "./components/Home";
+import Info from "./components/Info";
 
-const API= "https://api.themoviedb.org/3/discover/movie?api_key=fa89a5a01830500758ccda34c5a144c9&sort_by=popularity.desc&page=1";
-const SEARCH="https://api.themoviedb.org/3/search/movie?api_key=fa89a5a01830500758ccda34c5a144c9&query=";
-function App() {
-  const [movies,setMovies]=useState([]);
-  const [Search_term,setsearchterm]=useState("");
-  useEffect(()=>{
-    fetch(API)
-      .then((res)=>res.json())
-      .then((data)=>{
-        console.log(data);
-        setMovies(data.results);
-      });
-  },[]);
-  const handleOnSubmit=(e)=>{
-     e.preventDefault();
-     if(Search_term){
-       fetch(SEARCH+Search_term)
-         .then((res)=>res.json())
-         .then((data)=>{
-           setMovies(data.results);
-         })
-       setsearchterm("");
-     }
-  };
-
-  const handleOnChange=(e)=>{
-    setsearchterm(e.target.value);
-  }
-  // const movies=[1,2,3,4,5];
-  return (
-    <div className="App">
-    <header className="header"><h1>MOVIES APP</h1></header>
-    <form onSubmit={handleOnSubmit} className="form">
-       <input
-         className="search"
-         type="text"
-         placeholder="Search"
-         value={Search_term}
-         onChange={handleOnChange}
-       >
-       </input>
-    </form>
-     <div className="movie_container">
-       {movies.map((movie)=>(
-         <Movie key={movie.id} {...movie}/>
-       ))}
-      </div>
-    </div>
-  );
+function App(){
+  return(
+    <Router>
+    <Switch>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/Info/:id" exact component={Info}></Route> 
+    </Switch>
+    </Router>      
+  )
 }
 
 export default App;
-
-	
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 const IMG= "https://image.tmdb.org/t/p/w500/";
 
-const Movie=({title,overview,poster_path})=>
+const Movie=({id,title,overview,poster_path})=>
 <div className="Movie">
    <p>{title}</p>
    <img src={IMG+poster_path} alt=""></img>
