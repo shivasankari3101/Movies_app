c16da913b11bfd4148cb4f7aa24fae433a36caee
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-14 15:41:43 +0530

    Minor change

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ const IMG= "https://image.tmdb.org/t/p/w500/";
 const Movie=({title,overview,poster_path})=>
 <div className="Movie">
    <p>{title}</p>
-   <img src={IMG+poster_path}></img>
+   <img src={IMG+poster_path} alter={title}></img>
 </div>;
 
 export default Movie;
