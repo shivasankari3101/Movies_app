42e0fe9193807aa5420e7c3daff856f3228078cd
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-15 18:43:59 +0530

    Added some more info about movies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
+import { useEffect, useState } from 'react';
+import Movie from "./components/Movie";
+import "./App.css"
 
-import './App.css';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import Home from "./components/Home";
-import Info from "./components/Info";
+const API= "https://api.themoviedb.org/3/discover/movie?api_key=fa89a5a01830500758ccda34c5a144c9&sort_by=popularity.desc&page=1";
+const SEARCH="https://api.themoviedb.org/3/search/movie?api_key=fa89a5a01830500758ccda34c5a144c9&query=";
+function App() {
+  const [movies,setMovies]=useState([]);
+  const [Search_term,setsearchterm]=useState("");
+  useEffect(()=>{
+    fetch(API)
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data);
+        setMovies(data.results);
+      });
+  },[]);
+  const handleOnSubmit=(e)=>{
+     e.preventDefault();
+     if(Search_term){
+       fetch(SEARCH+Search_term)
+         .then((res)=>res.json())
+         .then((data)=>{
+           setMovies(data.results);
+         })
+       setsearchterm("");
+     }
+  };
 
-function App(){
-  return(
-    <Router>
-    <Switch>
-      <Route path="/" exact component={Home}></Route>
-      <Route path="/Info/:id" exact component={Info}></Route> 
-    </Switch>
-    </Router>      
-  )
+  const handleOnChange=(e)=>{
+    setsearchterm(e.target.value);
+  }
+  // const movies=[1,2,3,4,5];
+  return (
+    <div className="Home">
+    <header className="header"><h1>MOVIES APP</h1></header>
+    <form onSubmit={handleOnSubmit} className="form">
+       <input
+         className="search"
+         type="text"
+         placeholder="Search"
+         value={Search_term}
+         onChange={handleOnChange}
+       >
+       </input>
+    </form>
+     <div className="movie_container">
+       {movies.map((movie)=>(
+              <Movie key={movie.id}  {...movie}></Movie>
+       ))}
+      </div>
+     
+    </div>
+  );
 }
 
 export default App;
+
+	
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
 const IMG= "https://image.tmdb.org/t/p/w500/";
 
-const Movie=({id,title,overview,poster_path})=>
+const Movie=({id,title,overview,poster_path,vote_average,release_date})=>
 <div className="Movie">
    <p>{title}</p>
    <img src={IMG+poster_path} alt=""></img>
+   <p className="voteaverage"><span>{vote_average}</span></p>
+   <div className="overlay">
+      <h1>{title}</h1>
+      <p>{overview}</p>
+      <p>Release date: {release_date}</p>
+      <p>Vote Average: {vote_average}</p>
+   </div>
+  
 </div>;
 
 export default Movie;
